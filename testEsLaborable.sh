384e124095717ea35526d22d08eb3f6735622214
walczak-hector <65743857+walczak-hector@users.noreply.github.com>
2022-06-06 23:29:57 -0300

    Create testEsLaborable.sh

---
+++ testEsLaborable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Se imprime el texto y lo ingresado se lo asigna a la variable VAR
+echo "Ingrese el dia a verificar en formato ANSI (YYYY/MM/DD)"
+read VAR
+
+#Se invoca al script esLaborable con VAR como argumento
+./esLaborable $VAR
