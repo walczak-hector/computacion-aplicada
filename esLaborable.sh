3b5b772cb140400599bc9696dbf8db3e3d38285f
walczak-hector <65743857+walczak-hector@users.noreply.github.com>
2022-06-06 23:35:44 -0300

    Update esLaborable.sh

--- esLaborable.sh
+++ esLaborable.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
 esLaborable(){
-VAR = 2022/09/20
-DIA=`date -d $VAR +%u`
-DIA2=`date -d $VAR +%d/%m`
+
+#Asignamos a la variable dia el numero de la semana 0-7 del argumento ingresado)
+DIA=`date -d $1 +%u`
+
+#Asignamos a la variable el mes y el dia del argumento ingresado
+DIA2=`date -d $1 +%d/%m`
+
+#Buscamos el dia en la lista de feriados
 FERIADO=`cat feriados.txt | grep -w $DIA2`
-echo "$FERIADO"
+
+#Se verifica que el dia no sea sabado
 if [ $DIA = 6 ];then
-	echo "No es laborable"
+	echo "Sabado. No es laborable"
+
+#Se verifica que no sea domingo	
 elif [ $DIA = 7 ];then
-	echo "No es laborable"
+	echo "Domingo. No es laborable"
 elif [ $DIA = 0 ];then
-	echo "No es laborable"
+	echo "Domingo. No es laborable"
+
+#Se verifica que no sea feriado nacional
 elif [ "$FERIADO" ];then
-	echo "Es feriado, no es laborable"
+	echo "Es feriado nacional. No es laborable"
+
+#Se informa que los dias que no sean fines de semana y feriados son laborables
 else
 	echo "Es laborable"
 fi
 }
 
-esLaborable
+#Se invoca a la funcion
+esLaborable $1
