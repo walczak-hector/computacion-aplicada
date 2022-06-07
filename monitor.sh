aeacf4ce80e12b77cb9c84dc70ce210a600d79c8
walczak-hector <65743857+walczak-hector@users.noreply.github.com>
2022-06-07 00:54:56 -0300

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -4,10 +4,11 @@
 
 PID=`pidof $1`
 
-# Si se encontro el ID del proceso, se informa que se esta ejecutando. Caso contrario se informa que no se esta ejecutando.
+# Si se encontro el ID del proceso, se informa que se esta ejecutando. 
+# Caso contrario se informa que no se esta ejecutando y se informa un email.
 
 if [  "$PID" ];then
 	echo "$1 se esta ejecutando"
 else
-	echo "$1 no se esta ejecutando"
+	echo "$1 no se esta ejecutando" | tee /dev/stderr | /usr/bin/mailx "root@debian.up"
 fi
